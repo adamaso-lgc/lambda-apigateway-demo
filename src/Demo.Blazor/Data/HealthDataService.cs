d23af830ce8c43960410538b2ab59228472b9772
Andre Damaso <adamaso@letsgetchecked.com>
2023-10-24 09:52:13 +0100

    update start/stop stream

--- src/Demo.Blazor/Data/HealthDataService.cs
+++ src/Demo.Blazor/Data/HealthDataService.cs
@@ -9,18 +9,18 @@ public interface IHealthDataService
     event Action<HealthData> OnNewHealthDataReceived;
     Task StartConsuming(CancellationToken cancellationToken);
     void StopConsuming();
+    bool IsConsuming { get; } 
 }
 
 public class HealthDataService : IHealthDataService
 {
     private readonly ConsumerConfig _consumerConfig;
     private IConsumer<Ignore, string>? _kafkaConsumer;
-    private const string TopicName = "HealthDataTopic"; 
-    private readonly ILogger<HealthDataService> _logger;
+    private const string TopicName = "HealthDataTopic";
+    public bool IsConsuming { get; private set; } 
 
-    public HealthDataService(IConfiguration configuration, ILogger<HealthDataService> logger)
+    public HealthDataService(IConfiguration configuration)
     {
-        _logger = logger;
         _consumerConfig = new ConsumerConfig
         {
             GroupId = "health-data-group",
@@ -34,30 +34,48 @@ public class HealthDataService : IHealthDataService
     
     public async Task StartConsuming(CancellationToken cancellationToken)
     {
-        if (_kafkaConsumer != null) return; 
+        if (IsConsuming) return;
+        
+        IsConsuming = true;
         _kafkaConsumer = new ConsumerBuilder<Ignore, string>(_consumerConfig).Build();
-        _kafkaConsumer.Subscribe(TopicName);
+        await Task.Run(() => StartConsumerLoop(cancellationToken), cancellationToken);
+    }
 
+    private Task StartConsumerLoop(CancellationToken cancellationToken)
+    {
+        _kafkaConsumer!.Subscribe(TopicName);
         try
         {
-            while (!cancellationToken.IsCancellationRequested)
-            {
-                var consumeResult = await Task.Run(() => _kafkaConsumer.Consume(cancellationToken), cancellationToken);
-                var healthData = JsonSerializer.Deserialize<HealthData>(consumeResult.Message.Value);
-                OnNewHealthDataReceived?.Invoke(healthData);
-            }
+            while (!cancellationToken.IsCancellationRequested)  
+            {  
+                var result = _kafkaConsumer.Consume(cancellationToken);  
+                var healthData = JsonSerializer.Deserialize<HealthData>(result.Message.Value);
+                if (healthData != null)
+                {
+                    OnNewHealthDataReceived?.Invoke(healthData);
+                }
+            }  
         }
-        catch (OperationCanceledException)
+        catch (OperationCanceledException)  
+        {  
+            // Handle cancellation
+        }
+        finally
         {
-            _kafkaConsumer.Close();
+            IsConsuming = false;  // Reset this when you stop consuming
+            StopConsuming(); // Call StopConsuming to unsubscribe, close and dispose the consumer.
         }
+
+        return Task.CompletedTask;
     }
 
     public void StopConsuming()
     {
+        IsConsuming = false; 
+        _kafkaConsumer?.Unsubscribe(); // Unsubscribe first to stop consuming
         _kafkaConsumer?.Close();
-        _kafkaConsumer?.Dispose();  // Dispose the consumer
-        _kafkaConsumer = null;
+        _kafkaConsumer?.Dispose();
+        _kafkaConsumer = null; 
     }
 
 }
